d0648d7e6102ad5db8c1e4d3a3571c44e879d68c
Bradley Few <few.bradley@gmail.com>
2011-10-05 23:45:22 +0100

    ** Working version
    - Commited a working version. Still a few things to sort out
    - Fixed position causes element with width/padding set with percentages to go fruity. Needs work.
    - New files uploaded with this commit
    	- jQuery
    	- Modernizr

--- stickinglikeabitch.js
+++ stickinglikeabitch.js
@@ -1,13 +1,9 @@
 (function stickingLikeABitch(global) {
 		 
     var	doc = global.document,
-			elemParent = getEl('photo'),
-		 	img = getTag({ tag: 'img', context: elemParent, first: true }), 
-			ul = getEl('feeder'),
-		 	thumbs = getTag({ tag: 'a', context: ul }),
-			len = thumbs.length, 
-			src;
-	
+			container = getEl('contain'),
+			parentElem = container.getElementsByTagName('article'),
+			header = document.getElementsByTagName('header')[0].clientHeight;
 	
 	/**
 	 * Following method is short hand for document.getElementById
@@ -46,5 +42,49 @@
 		return classElements;
 		
 	};
+	
+	
+	
+	jQuery(window).bind('scroll', function(e){
+			var scrolling = jQuery(window).scrollTop();
+		
+		
+			for (i = 0; i < parentElem.length; i++) {
+				
+				// Grabbing some numbers here. What we need to know is when our user has scrolled past
+				// a certain position. These set of variables say if our user scrolls past the value
+				// of our elements offseTop minus the height of our header minus 300px.
+				var elem = parentElem[i],
+					elemOffSet = elem.offsetTop,
+					elemHeight = elem.clientHeight,
+					space = 50, // This determines how much space after the header
+					totalSpace = elemOffSet - header - space, // x = y(w-z) -- First attempt ever at an algorithm haha
+					totalFigure = elemOffSet + elemHeight,
+					stickyElem = elem.getElementsByTagName('div')[0], // We know its our first div element:)
+					stickyElemHeight = stickyElem.clientHeight,
+					stickyElementWidth = stickyElem.clientWidth;
+					
+					
+					// Our current width is set in percentages for a responsive effect. When we apply a fixed position to it, it seems
+					// to expand to it touches the next element. Only width wise.
+					//stickyElem.style.width = stickyElem.clientWidth+'px';	
+				
+				// Chuck in a check to see which one of our elements are closent to the top of the window
+				if (scrolling >= elemOffSet - 100) {
+					if (scrolling >= totalFigure - stickyElemHeight - 210) {
+						stickyElem.className = 'stickyElem release';
+					} else {
+						stickyElem.className = 'stickyElem fixed';
+					}
+				} else {
+					stickyElem.className = 'stickyElem';
+				}
+			}
+		
+	})
+	
+	
+	
+	
 		
 })(this) // We pass in 'this' and reference it as 'global' object (in the browser world the global object is the Window object)
